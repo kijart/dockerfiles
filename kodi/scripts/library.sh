c0db299306df509e1dca43842c36840a5396fd97
Luis Miguel Vicente Fuentes <kijart@gmail.com>
2018-03-03 13:25:53 +0100

    Add kodi library script to be used as a crontab script

---
+++ kodi/scripts/library.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# crontab script
+# m h  dom mon dow   command
+# 0,30 * * * * sh /home/<UserName>/dockerfiles/kodi/scripts/library.sh
+
+workingDirectory=$(dirname $0)
+
+$workingDirectory/cleanLibrary.sh
+$workingDirectory/updateLibrary.sh
